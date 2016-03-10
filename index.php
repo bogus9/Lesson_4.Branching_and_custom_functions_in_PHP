48242cabfbee0d6176578bdfa025207c9eb3f7ff
Pavel Galyshev <bogus9@mail.ru>
2016-03-11 01:42:34 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+    error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
+    ini_set('display_errors', 1);
+
+    
+    $my_name = 'pavel2345';
+    
+    if($my_name=='pavel')
+    {
+        if($my_name=='pavel')
+        {
+            if($my_name=='pavel')
+            {
+                echo 'yes'; //1 => TRUE
+            }
+        }
+    }
+    elseif($my_name=='pavel123')
+        echo 'pavel 123';
+    
+    elseif($my_name=='pavel2345')
+        echo 'pavel2345';
+    
+    else
+        echo 'no'; // 0 => FALSE
+    
+?>
